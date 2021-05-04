678c66ed1e7c22d2691b576bdad524bc5e6a7464
pengc-1 <63624255+pengc-1@users.noreply.github.com>
2021-05-04 17:21:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,24 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
+mkdir -p package/custom
+cd package/custom
+git clone --depth=1 https://github.com/openwrt-develop/luci-theme-atmaterial
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+#git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
+git clone --depth=1  https://github.com/pymumu/luci-app-smartdns
+git clone --depth=1 https://github.com/pymumu/openwrt-smartdns
+git clone --depth=1   https://github.com/jerrykuku/luci-theme-argon
+#git clone --depth=1 https://github.com/vernesong/OpenClash
+#git clone --depth=1 https://github.com/Lienol/openwrt-package
+git clone --depth=1 https://github.com/fw876/helloworld
+git clone --depth=1 https://github.com/kenzok8/openwrt-packages
+git clone --depth=1 https://github.com/kenzok8/small
+svn checkout https://github.com/OpenWrt-Actions/openwrt-package-lienol/trunk/lienol/luci-app-passwall passwall
+cd -
+
+./scripts/feeds update -a
+./scripts/feeds install -a
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
